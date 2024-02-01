628b4fc9101f884b8fee865bb9a9b12f945be9ea
Fang <74826581+mn1shi@users.noreply.github.com>
2024-02-01 09:09:59 +0000

    Video Player Time

--- Assets/Scripts/clipcontrol.cs
+++ Assets/Scripts/clipcontrol.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Video;
 
 public class clipcontrol : MonoBehaviour
 {
+    //Video Play Control
+    [SerializeField] private TMP_Text VideoTimeTextBox;
+    private const float videoPlayTime = 30f; // Play each video for 60 seconds
+
     // Start is called before the first frame update
     public VideoClip[] videoClips = new VideoClip[10];
     public AudioClip[] audioClips = new AudioClip[10];
@@ -14,14 +19,15 @@ public class clipcontrol : MonoBehaviour
     private int currentClipIndex = 0;
     private int maxClipIndex = 9;
 
-    private const float videoPlayTime = 30f; // Play each video for 60 seconds
+
+    
     private const float restPeriod = 10f; // Rest period of 15 seconds
 
 
     void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
-        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
 
         StartCoroutine(PlayVideoSequence());
     }
