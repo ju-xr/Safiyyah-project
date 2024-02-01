3393f7bed5230c1ad4fe334dd0ce47216d737567
Ju Zhang <chenzi217@gmail.com>
2024-02-01 13:55:20 +0000

    csv initial

---
+++ Assets/Scripts/DataCollection.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCollection : MonoBehaviour
+{
+    public UI_Function settingData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SettingParameterSaver()
+    {
+        // Save setting panel data INTO file .csv when start button pressed 
+
+        //partcipant code, audio order, 
+    }
+
+}
